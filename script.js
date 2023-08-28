2ad589d86a63aaa60e536ccf07c1887580d54852
Massoud Rahmani <mrahmani@LABA.PSWAS.LOCAL>
2023-08-28 16:04:52 -0700

    finished lab

--- script.js
+++ script.js
@@ -29,28 +29,63 @@ const pokemon = Object.freeze([
 // ==========================TASKS============================= 
 
 //1. find pokemon objects where the id is evenly divisible by 3.
-// const idIsEvenlyDivisable = pokemon.filter((i)=>{
-//     if(i.id % 3 === 0){
-//         console.log(i.id, i.name)
-//     }
-// });
+const idIsEvenlyDivisable = pokemon.filter((i)=>{
+    if(i.id % 3 === 0){
+        // console.log(i.id, i.name)
+    }
+});
 
 // 2. find the pokemon objects that are "fire" type.
-// const typeFinder = pokemon.filter((t)=>{
-//     if(t.types.includes("fire")){
-//         console.log(t.name, t.types);
-//     }
-// });
+const typeFinder = pokemon.filter((t)=>{
+    if(t.types.includes("fire")){
+        // console.log(t.name, t.types);
+    }
+});
 
 //3. an array with just the names of the pokemon whose only type is poison
+const findTypeOfPoison = pokemon.filter((type)=> {
+    if(type.types.includes("poison")){
+        // console.log(type.name, type.types);
+    }
+});
+// USING THE MAP FUNCTION
+// const findTypeOfPoison = pokemon.map((type)=>{
+//     if(type.types.includes("poison"))
+//     console.log(type.name, type.types)
+// });
+
 
 //4. an array of pokemon objects that have more than one type
+const findMoreTypes = pokemon.filter((type)=>{
+    if(type.types.length > 1){
+        // console.log(type.name, type.types, "has more than one type");
+    }
+});
 
 //5. an array with just the names of pokemon with an id greater than 99
+const namesGreatherThan = pokemon.filter((greatherNames)=>{
+    if(greatherNames.id > 99){
+        // console.log(greatherNames.name + " has an id of " + greatherNames.id + " and is greater than 99");
+    }
+});
 
 //6. an array with just the names of the pokemon
+const justNames = pokemon.filter((names)=>{
+    // console.log(names.name);
+});
 
 //7. an array containing just the first type of all the pokemon whose second type is "flying"
-
+const containingType = pokemon.filter((firsType)=>{
+    if(firsType.types.length > 1){
+        // console.log(firsType.types[0]);
+    }
+});
 
 //8. a count of the number of pokemon that are "normal" type
+let count = 0;
+pokemon.forEach((typeNumber)=>{
+    if(typeNumber.types.includes("normal")){
+        count++;
+    };
+});
+// console.log(`Number of Pokémon with type "normal": ${count}`);
